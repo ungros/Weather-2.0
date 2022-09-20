429f46ac830e6a77147c2a861edc741d4f170866
ungros <82379236+ungros@users.noreply.github.com>
2022-09-20 22:07:49 +0300

    Prep

---
+++ Weather 2.0/Tools/Resources.swift
@@ -0,0 +1,31 @@
+//
+//  Resources.swift
+//  Weather 2.0
+//
+//  Created by Roman on 08.08.2022.
+//
+
+import UIKit
+
+enum R {
+    
+    enum Colors {
+        
+    }
+    enum Strings {
+        
+    }
+    
+    enum Images {
+        enum TabBar {
+            
+        }
+    }
+    
+//    enum Fonts {
+//        static func helveticaRegularr(with size: CGFloat) -> UIFont {
+//            UIFont(name: "Helvetica", size: size) ?? UIFont
+//        }
+    }
+    
+
