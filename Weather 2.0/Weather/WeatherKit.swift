c121f6d603d90869f1c747fc40effe3c820f5bb0
ungros <82379236+ungros@users.noreply.github.com>
2022-10-24 16:11:39 +0300

    CLL

--- Weather 2.0/Weather/WeatherKit.swift
+++ Weather 2.0/Weather/WeatherKit.swift
@@ -5,16 +5,32 @@
 //  Created by Roman on 04.10.2022.
 //
 
+import CoreLocation
 import UIKit
 import WeatherKit
 
+
 @available(iOS 16.0, *)
-final class Weather {
+open class Weather: UIViewController, CLLocationManagerDelegate {
+   
+    let locationManager = CLLocationManager()
     let service = WeatherService()
     
     
     func getUserLocation() {
-        
+        locationManager.stopUpdatingLocation()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func getWeather(location: CLLocation) {
+        Task {
+            do {
+                let result = try await service.weather(for: location)
+            } catch {
+                print(String(describing: error))
+            }
+        }
     }
 }
 
