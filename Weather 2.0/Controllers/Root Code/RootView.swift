b431d256b9ddc1b53e87c623365dbeaa97dcc417
ungros <82379236+ungros@users.noreply.github.com>
2022-10-27 14:54:34 +0300

    RootView

--- Weather 2.0/Controllers/Root Code/RootView.swift
+++ Weather 2.0/Controllers/Root Code/RootView.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class RootView: UIView {
     
+    let blurView: UIVisualEffectView = {
+        let view = UIVisualEffectView()
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        constraintViews()
+        configureAppearence()
+	
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 @objc extension RootView {
