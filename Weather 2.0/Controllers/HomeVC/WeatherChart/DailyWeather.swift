333a25be9cf7e8925c2f2663b7ed96b40e5e9100
ungros <82379236+ungros@users.noreply.github.com>
2022-11-23 17:46:19 +0300

    StartDailyScroll

---
+++ Weather 2.0/Controllers/HomeVC/WeatherChart/DailyWeather.swift
@@ -0,0 +1,28 @@
+//
+//  DailyWeather.swift
+//  Weather 2.0
+//
+//  Created by Roman on 23.11.2022.
+//
+
+import UIKit
+import SwiftUI
+
+final class HoursCollection: RootCollectionView {
+    
+    let collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.backgroundColor = .red
+        return collectionView
+    }()
+}
+
+extension HoursCollection {
+    
+    override func setupViews() {}
+    override func constraintViews() {}
+    override func configureAppearence() {
+    
+    }
+}
+
